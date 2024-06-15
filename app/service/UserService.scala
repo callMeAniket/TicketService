583ee99d2876d48bade3fe9bc9980e3c5d918277
callMeAniket <aniket.s@payoda.com>
2024-06-15 18:03:56 +0530

    Ticket Service

--- app/service/UserService.scala
+++ app/service/UserService.scala
@@ -9,6 +9,7 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class UserService @Inject()(implicit ec: ExecutionContext, ws: WSClient){
   def getUserByToken(token: String): Future[User] = {
+    println("calling api with token  :  + " + token )
     ws.url("http://userservice:9000/usersByToken")
       .addHttpHeaders("Content-Type" -> "application/json")
       .addHttpHeaders("token" -> token)
@@ -16,12 +17,11 @@ class UserService @Inject()(implicit ec: ExecutionContext, ws: WSClient){
       .flatMap { response =>
         response.status match {
           case 200 =>
-
            println("successful")
             Future.successful(response.json.as[User])
           case ex =>
             println(response)
-            println(response.status)
+            println(response.body)
             println("API called failed")
             Future.failed(new Exception("API called failed " + ex))
         }
